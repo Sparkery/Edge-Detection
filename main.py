6d9b26d239d2ac6fd40ec88493d3d3db6bfdaf3d
Sparkery <darkyx65@gmail.com>
2013-03-21 22:09:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,15 @@
 #January 31, 2012
 #Image Processing Computer Vision
 
+#Prints to file directed in the terminal. Otherwise the file contents are printed to the standard output stream
+
 from sys import argv
 from math import atan2, sin, cos
 
 def index(row, col) :
   return rows * col + row
+
+#Opens .ppm file and puts data into list
 
 image = open(raw_input("File name .ppm"), "r").read().split()
 rows = int(image[1])
@@ -20,20 +24,21 @@ print("255")
 
 array = []
 
+#Converts file to grayscale
+
 for X in range(0, len(rgb), 3):
 	temp = int(0.299 * rgb[X] + 0.587 * rgb[X + 1] + 0.114 * rgb[X + 2])
 	array.append(temp)
-	#print(temp, temp, temp)
+
+#Gaussian smoothing is applied to the grayscale
 
 array2 = []
 for Y in range(cols):
 	for X in range(rows):
 		if X == 0 or X == rows - 1 or Y == 0 or Y == cols - 1:
-			#print(array[index(X, Y)], array[index(X,Y)], array[index(X,Y)])
 			array2.append(array[index(X, Y)])
 			continue
-		#val = int((4*(array[index(X,Y)])+2*(array[index(X+1,Y)]+array[index(X-1,Y)]+array[index(X,Y+1)]+array[index(X,Y-1)])+ (array[index(X-1, Y-1)]+array[index(X-1, Y+1)] + array[index(X+1, Y-1)] + array[index(X+1,Y +1)]))/16)
-
+		
 		tl = array[index(X - 1, Y - 1)] * 1
 		tc = array[index(X - 1, Y)] * 2
 		tr = array[index(X - 1, Y + 1)] * 1
@@ -45,17 +50,17 @@ for Y in range(cols):
 		br = array[index(X + 1, Y + 1)] * 1
 		val = int((tl + tc + tr + cl + cc + cr + bl + bc + br) / 16.0)
 		
-		#print(val, val, val)
 		array2.append(val)
 
 gx = 0
 gy = 0
 grr = []
 
+#Changes in vertical and horizontal intensity are calculated
+
 for Y in range(cols):
 	for X in range(rows):
 		if X == 0 or X == rows - 1 or Y == 0 or Y == cols - 1:
-			#print(array2[index(X, Y)], array2[index(X,Y)], array2[index(X,Y)])
 			grr.append(array[index(X, Y)])
 		else:
 
@@ -82,16 +87,19 @@ for Y in range(cols):
 			gx = int(tl1 + tr1 + cl1 + cr1 + bl1 + br1)
 			gy = int(tl2 + tc2 + tr2 + bl2 + bc2 + br2)
 			val = abs(gx) + abs(gy)
-#print(array2[index(X, Y)], array2[index(X,Y)], array2[index(X,Y)])
+
 			grr.append(val)
+
+#Normalizes intensity values
 
 m = float(max(grr))
 for X in range(len(grr)):
 	grr[X] = grr[X] / m
 
+#Identifies pixels with change of intensity in the top 85% to be edges
+
 for X in range(len(grr)):
 	if grr[X] > 0.15:
 		print(0, 0, 0)
 	else:
-		#print(rgb[X * 3], rgb[X * 3 + 1], rgb[X * 3 + 2]
 		print(255, 255, 255)
